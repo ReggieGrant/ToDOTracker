2cc627c90f1f4bc5e2d72c80c532767dbe386aae
ReggieGrant <grantreggie09@gmail.com>
2026-06-22 18:08:32 -0700

    Initial Commit

---
+++ ToDOTracker/ToDOTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDOTrackerApp.swift
+//  ToDOTracker
+//
+//  Created by Reginald Grant on 6/22/26.
+//
+
+import SwiftUI
+
+@main
+struct ToDOTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
